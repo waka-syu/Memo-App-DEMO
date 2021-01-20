056b4c66edc1ebb3c0150193bf6327af3e901d0a
waka-syu <hamutailang059@gmail.com>
2021-01-20 12:24:24 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :search_genre_memo
 
@@ -9,6 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 
   def search_genre_memo
     @q = Memo.ransack(params[:q])
